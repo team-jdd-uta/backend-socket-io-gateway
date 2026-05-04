1e5a6bbaa663d27dc9d3c61d9844496610f210f2
hyeonyway <kimhm1219@gmail.com>
2026-05-04 15:13:45 +0900

    feat: expose prometheus metrics

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ const { Server } = require('socket.io');
 const config = require('./config');
 const { ensureRedisClients, closeRedisClients } = require('./redis/client');
 const { createChatGateway } = require('./gateway/chatGateway');
+const { createGatewayMetrics } = require('./metrics/prometheusMetrics');
 const { installGracefulShutdown } = require('./lifecycle/gracefulShutdown');
 
 async function main() {
   const redisClients = await ensureRedisClients();
+  const gatewayMetrics = createGatewayMetrics();
   let draining = false;
 
   const server = http.createServer((req, res) => {
     if (req.url === '/health') {
       res.writeHead(draining ? 503 : 200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ ok: !draining }));
+      return;
+    }
+
+    if (req.url === '/metrics') {
+      res.writeHead(200, { 'Content-Type': 'text/plain; version=0.0.4; charset=utf-8' });
+      res.end(gatewayMetrics.render());
       return;
     }
 
@@ -32,6 +40,7 @@ async function main() {
   const gateway = createChatGateway({
     io,
     redisClients,
+    metrics: gatewayMetrics,
   });
 
   installGracefulShutdown({
